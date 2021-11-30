01341238d8383acb95478c23487e0f43290d5d93
Dmitry Platonoff <dmitry@platonoff.com>
2021-11-29 22:48:06 -0500

    Home and content page placeholders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContentPageComponent } from './pages/content-page/content-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'content/:category/:document',
+    component: ContentPageComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/content-page/content-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ovr-content-page',
+  template: `
+    <p>
+      content-page works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ContentPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
