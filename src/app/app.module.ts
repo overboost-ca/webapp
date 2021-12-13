78e159e81d3fbf5c84558eb2cae1774cf1e7a8ae
Dmitry Platonoff <dmitry@platonoff.com>
2021-12-12 20:38:03 -0500

    More discreet slide counters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -23,7 +22,6 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatBadgeModule,
     MatButtonModule,
     MatCardModule,
     MatIconModule,
