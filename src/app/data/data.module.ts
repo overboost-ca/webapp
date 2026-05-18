798a2b14ebcc42e68082df18b31ddabe2f4c95b3
Dmitry Platonoff <dmitry@platonoff.com>
2026-05-18 18:30:12 -0400

    Angular 16 > 17

--- src/app/data/data.module.ts
+++ src/app/data/data.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { CategoryService } from './category.service';
 import { DocumentService } from './document.service';
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ],
-  providers: [CategoryService, DocumentService]
-})
+@NgModule({ declarations: [], imports: [CommonModule], providers: [CategoryService, DocumentService, provideHttpClient(withInterceptorsFromDi())] })
 export class DataModule { }
